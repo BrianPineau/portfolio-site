0d59b97811eb50b265550620650b368ea9095881
Brian Pineau <pinoub@gmail.com>
2019-08-09 10:53:28 -0500

    Finished project section layout.

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 
 
-const lead = document.querySelector('header');
+const head = document.querySelector('header');
 const noShow = document.querySelector('.arrow-box');
-let position = 0;
+const skillText = document.querySelector('.skills-text');
+
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 100) {
-        lead.classList += ' shrink';
+        head.classList += ' shrink';
         noShow.style.display = 'none';
 
     } 
 });
  
 
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 1000) {
+        skillText.classList += ' skill-scroll'; 
+    }
+});
