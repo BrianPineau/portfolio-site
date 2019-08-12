69bd0b843cf0b3d5d230a0886d0e32d1a57e3ef3
Brian Pineau <pinoub@gmail.com>
2019-08-12 18:26:44 -0500

    scroll animation added in skill section

--- main.js
+++ main.js
@@ -13,9 +13,27 @@ window.addEventListener('scroll', function () {
     } 
 });
  
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.skill-1',
+    triggerHook: 0.9,
+})
+.setClassToggle('.skill-1', 'show')
+.addTo(controller);
 
-window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 1000) {
-        skillText.classList += ' skill-scroll'; 
-    }
-});
+
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: '.skill-2',
+    triggerHook: 0.8,
+})
+.setClassToggle('.skill-2', 'show')
+.addTo(controller);
+
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: '.skill-3',
+    triggerHook: 0.8,
+})
+.setClassToggle('.skill-3', 'show')
+.addTo(controller);
